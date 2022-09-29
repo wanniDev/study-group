75bd9d0c2195d9af2868b0df989e0c1c29bbbc42
wanniDev <wannidev0928@gmail.com>
2022-09-29 20:56:40 +0900

    feat: project init

---
+++ src/test/java/me/spring/studygroup/StudygroupApplicationTests.java
@@ -0,0 +1,13 @@
+package me.spring.studygroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudygroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
