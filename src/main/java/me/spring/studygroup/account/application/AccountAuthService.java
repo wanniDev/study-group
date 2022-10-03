8df280a3f22c3e1f5ed9b00c12405d2717b101cc
wanniDev <wannidev0928@gmail.com>
2022-10-03 23:41:16 +0900

    Feat: 로그인 기능 구현
    - 우선 authenticationManager을 통한 인증이 아니라, SecurityContext를 직접 사용

--- src/main/java/me/spring/studygroup/account/application/AccountAuthService.java
+++ src/main/java/me/spring/studygroup/account/application/AccountAuthService.java
@@ -1,5 +1,10 @@
 package me.spring.studygroup.account.application;
 
+import java.util.List;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import me.spring.studygroup.account.domain.Account;
@@ -8,6 +13,10 @@ import me.spring.studygroup.account.domain.Account;
 public class AccountAuthService {
 
 	public void login(Account account) {
-
+		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+			account.getNickname(),
+			account.getPassword(),
+			List.of(new SimpleGrantedAuthority("ROLE_USER")));
+		SecurityContextHolder.getContext().setAuthentication(token);
 	}
 }
