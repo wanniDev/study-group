467bb406b35db50da66e09965dd0a8e062b97f2e
wanniDev <wannidev0928@gmail.com>
2022-10-06 16:26:50 +0900

    Feat: 프로필 편집 페이지 뷰, 컨트롤러 구현

--- src/main/java/me/spring/studygroup/account/presentation/AccountProfileController.java
+++ src/main/java/me/spring/studygroup/account/presentation/AccountProfileController.java
@@ -1,5 +1,6 @@
 package me.spring.studygroup.account.presentation;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +10,14 @@ import lombok.RequiredArgsConstructor;
 import me.spring.studygroup.account.application.AccountInfoFinderService;
 import me.spring.studygroup.account.domain.Account;
 import me.spring.studygroup.account.infrastructure.security.AuthAccount;
+import me.spring.studygroup.account.presentation.form.ProfileForm;
 
 @Controller
 @RequiredArgsConstructor
 public class AccountProfileController {
 
 	private final AccountInfoFinderService accountInfoFinderService;
+	private final ModelMapper modelMapper;
 
 	@GetMapping("/profile/{nickname}")
 	public String viewProfile(@PathVariable String nickname, Model model, @AuthAccount Account account) {
@@ -22,5 +25,12 @@ public class AccountProfileController {
 		model.addAttribute(accountToView);
 		model.addAttribute("isOwner", accountToView.equals(account));
 		return "account/profile";
+	}
+
+	@GetMapping("/settings/profile")
+	public String updateProfileForm(@AuthAccount Account account, Model model) {
+		model.addAttribute(account);
+		model.addAttribute(modelMapper.map(account, ProfileForm.class));
+		return "settings/profile";
 	}
 }
