03360797ee79f12dd60f31384d2353c5710a6ac7
wanniDev <wannidev0928@gmail.com>
2022-10-09 00:53:16 +0900

    Test: 닉네임 변경 테스트코드 추가

--- src/test/java/me/spring/studygroup/account/presentation/AccountProfileControllerTest.java
+++ src/test/java/me/spring/studygroup/account/presentation/AccountProfileControllerTest.java
@@ -37,7 +37,7 @@ class AccountProfileControllerTest {
 	@Test
 	@WithAccount("wannidev")
 	@DisplayName("프로필 수정 폼")
-	void updateAccountForm() throws Exception {
+	void updateProfileForm() throws Exception {
 		mockMvc.perform(get("/settings/profile"))
 			.andExpect(status().isOk())
 			.andExpect(model().attributeExists("account"))
@@ -117,5 +117,45 @@ class AccountProfileControllerTest {
 			.andExpect(model().hasErrors())
 			.andExpect(model().attributeExists("passwordForm"))
 			.andExpect(model().attributeExists("account"));
+	}
+
+	@Test
+	@WithAccount("wannidev")
+	@DisplayName("닉네임 수정 폼")
+	void updateAccountForm() throws Exception {
+		mockMvc.perform(get("/settings/account"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeExists("account"))
+			.andExpect(model().attributeExists("nicknameForm"));
+	}
+
+	@Test
+	@WithAccount("wannidev")
+	@DisplayName("닉네임 수정하기 - 입력값 정상")
+	void updateAccount_success() throws Exception {
+		String newNickname = "whiteship";
+		mockMvc.perform(post("/settings/account")
+				.param("nickname", newNickname)
+				.with(csrf()))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(redirectedUrl("/settings/account"))
+			.andExpect(flash().attributeExists("message"));
+
+		assertNotNull(accountRepository.findByNickname("whiteship"));
+	}
+
+	@Test
+	@WithAccount("wannidev")
+	@DisplayName("닉네임 수정하기 - 입력값 에러")
+	void updateAccount_failure() throws Exception {
+		String newNickname = "¯\\_(ツ)_/¯";
+		mockMvc.perform(post("/settings/account")
+				.param("nickname", newNickname)
+				.with(csrf()))
+			.andExpect(status().isOk())
+			.andExpect(view().name("settings/account"))
+			.andExpect(model().hasErrors())
+			.andExpect(model().attributeExists("account"))
+			.andExpect(model().attributeExists("nicknameForm"));
 	}
 }
