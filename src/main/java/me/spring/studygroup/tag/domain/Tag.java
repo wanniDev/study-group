f2090980f5636454c917e171e0c44716c83d06b1
wanniDev <wannidev0928@gmail.com>
2022-10-14 00:04:49 +0900

    Feat: Tag 도메인 구현

---
+++ src/main/java/me/spring/studygroup/tag/domain/Tag.java
@@ -0,0 +1,31 @@
+package me.spring.studygroup.tag.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tag {
+	@Id
+	@Column(name = "tag_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true, nullable = false)
+	private String title;
+}
