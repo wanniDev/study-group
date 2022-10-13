1704c02b77774a621277b9cc6a182e363cd2e3c9
wanniDev <wannidev0928@gmail.com>
2022-10-14 00:42:36 +0900

    Feat: 태그 세팅 뷰 구현

--- src/main/java/me/spring/studygroup/tag/domain/TagRepository.java
+++ src/main/java/me/spring/studygroup/tag/domain/TagRepository.java
@@ -1,2 +1,11 @@
-package me.spring.studygroup.tag.domain;public interface TagRepository {
+package me.spring.studygroup.tag.domain;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface TagRepository extends JpaRepository<Long, Tag> {
+	@Query("select t.title from Tag t")
+	List<String> findTagsTitle();
 }
