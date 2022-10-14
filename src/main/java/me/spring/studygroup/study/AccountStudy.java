df272730aed210c23e9d4cda7e60d452f27a2c34
wanniDev <wannidev0928@gmail.com>
2022-10-15 01:02:49 +0900

    Feat: Study 도메인 엔티티 구현
    - 스터디와 계정과의 연관관계는 다대다
    - 계정을 통해 스터디를 조회하는 경우는 흔하지 않으므로 아직은 굳이 양방향 관계로 두지는 않았다.

---
+++ src/main/java/me/spring/studygroup/study/AccountStudy.java
@@ -0,0 +1,37 @@
+package me.spring.studygroup.study;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.spring.studygroup.account.domain.Account;
+
+@Entity
+@Table(name = "account_study")
+@Getter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AccountStudy {
+	@Id
+	@Column(name = "account_study_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="account_id")
+	private Account account;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="study_id")
+	private Study study;
+}
---
+++ src/main/java/me/spring/studygroup/study/Study.java
@@ -0,0 +1,86 @@
+package me.spring.studygroup.study;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import me.spring.studygroup.account.domain.Account;
+import me.spring.studygroup.account.infrastructure.security.AccountPrincipal;
+import me.spring.studygroup.tag.domain.Tag;
+import me.spring.studygroup.zone.domain.Zone;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Study {
+
+    @Id
+    @Column(name = "study_id")
+    @GeneratedValue
+    private Long id;
+
+    @ManyToMany
+    private Set<Account> managers = new HashSet<>();
+
+    @OneToMany
+    private Set<AccountStudy> members = new HashSet<>();
+
+    @Column(unique = true)
+    private String path;
+
+    private String title;
+
+    private String shortDescription;
+
+    @Lob
+    @Basic(fetch = FetchType.EAGER)
+    private String fullDescription;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String image;
+
+    @ManyToMany
+    private Set<Tag> tags = new HashSet<>();
+
+    @ManyToMany
+    private Set<Zone> zones = new HashSet<>();
+
+    private LocalDateTime publishedDateTime;
+
+    private LocalDateTime closedDateTime;
+
+    private LocalDateTime recruitingUpdatedDateTime;
+
+    private boolean recruiting;
+
+    private boolean published;
+
+    private boolean closed;
+
+    private boolean useBanner;
+
+    private int memberCount;
+
+}
