28c0f033ac31a8eb25cbb6e144dac00b2ebd0c71
wanniDev <wannidev0928@gmail.com>
2022-10-14 12:26:27 +0900

    Test: 관심 태그 테스트 작성

--- src/main/java/me/spring/studygroup/tag/domain/Tag.java
+++ src/main/java/me/spring/studygroup/tag/domain/Tag.java
@@ -12,6 +12,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
@@ -20,6 +21,7 @@ import lombok.Setter;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Tag {
 	@Id
 	@Column(name = "tag_id")
