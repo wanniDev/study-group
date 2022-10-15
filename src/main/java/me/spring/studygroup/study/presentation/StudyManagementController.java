9a534076df0e48fd13254ab4a21faf09148a3afc
wanniDev <wannidev0928@gmail.com>
2022-10-15 23:15:52 +0900

    Feat: 스터디 팀원 조회

---
+++ src/main/java/me/spring/studygroup/study/presentation/StudyManagementController.java
@@ -0,0 +1,26 @@
+package me.spring.studygroup.study.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import lombok.RequiredArgsConstructor;
+import me.spring.studygroup.account.domain.Account;
+import me.spring.studygroup.account.infrastructure.security.AuthAccount;
+import me.spring.studygroup.study.domain.Study;
+import me.spring.studygroup.study.presentation.application.StudyFinderService;
+
+@Controller
+@RequiredArgsConstructor
+public class StudyManagementController {
+	private final StudyFinderService studyFinderService;
+
+	@GetMapping("/study/{path}/members")
+	public String viewStudyMembers(@AuthAccount Account account, @PathVariable String path, Model model) {
+		Study study = studyFinderService.findByPath(path);
+		model.addAttribute(account);
+		model.addAttribute(study);
+		return "study/members";
+	}
+}
